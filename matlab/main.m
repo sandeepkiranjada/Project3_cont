02b58cc5bce400297a9edcbe869cc8ea41d52924
sandeepkiranjada <36130965+sandeepkiranjada@users.noreply.github.com>
2019-04-11 22:43:27 -0400

    v2

--- matlab/main.m
+++ matlab/main.m
@@ -8,9 +8,21 @@ kp = 1;
 kd = 1;
 d = 10;
 
-ui_end_1 = @(xi,vi,xip1,vip1) ( kp * ( -xi + xip1 - d ) + kd * ( -vi + vip1 ) );
+N = 10; % Platoon size with Leader
 
-ui_mid_1 = @(xi,vi,xim1,xip1,vim1,vip1) ( kp * ( -2*xi + xim1 + xip1 ) + kd * ( -2*vi + vim1 + vip1 ) );
+N_folw = N-1;
+
+A = diag(mod((1:N_folw*2),2)==1,1)*1;
+
+B = zeros(2*N-1,N);
+for n=2:2:2*N_folw
+    B(n,n/2) = 1;
+end
+B(2*N-1,N) = 1;
 
 
-ui_first_1 = @(xi,vi,xim1,xip1,vim1,vip1) ( kp * ( -2*xi + xim1 + xip1 ) + kd * ( -2*vi + vim1 + vip1 ) );
+Ki = [-kp kp -kd kd 0];
+
+Ri = @(vn) [d d 0 0 vn]';
+
+Yi = @(xi,vi,xim1,xip1,vim1,vip1) [xip1-xi xi-xim1 vip1-vi vi-vim1 vi]';
