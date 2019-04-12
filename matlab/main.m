8fc7c76ee1631c93a3ccb2840dd6125a4354828a
sandeepkiranjada <36130965+sandeepkiranjada@users.noreply.github.com>
2019-04-11 23:05:11 -0400

    v3

--- matlab/main.m
+++ matlab/main.m
@@ -15,14 +15,15 @@ N_folw = N-1;
 A = diag(mod((1:N_folw*2),2)==1,1)*1;
 
 B = zeros(2*N-1,N);
+C = zeros(N_folw*5-3,2*N-1);
 for n=2:2:2*N_folw
     B(n,n/2) = 1;
 end
 B(2*N-1,N) = 1;
 
 
-Ki = [-kp kp -kd kd 0];
+Ki = [kpi kdi -kpi -kdi 0];
 
 Ri = @(vn) [d d 0 0 vn]';
 
-Yi = @(xi,vi,xim1,xip1,vim1,vip1) [xip1-xi xi-xim1 vip1-vi vi-vim1 vi]';
+Yi = @(Ci,Xi) Ci*Xi;
