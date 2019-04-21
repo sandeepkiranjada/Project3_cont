0a30ba62185571c46e5b2b061c2ff7d5bbf15b4a
sandeepkiranjada <36130965+sandeepkiranjada@users.noreply.github.com>
2019-04-20 21:11:39 -0400

    limits on Vel and acc

--- matlab/Xdot.m
+++ matlab/Xdot.m
@@ -12,7 +12,23 @@ G = zeros(length(U),1);
 G(end) = 1;
 
 
+maxV = 44.704; % 100 mph in m/s
+minV = -6.7056; % -15 mph in m/s
+
+maxA = 13.4112; % 30 mph/s in m/s^2
+minA = -13.4112; % -30 mph/s m/s^2
+
+N = length(X);
+
+Xd_max  = ((mod(1:N,2)==0)*maxV + (mod(1:N,2)==1)*maxA)';
+Xd_min  = ((mod(1:N,2)==0)*minV + (mod(1:N,2)==1)*minA)';
+
+
 Xd = A * X + U + G*W(t);
+
+Xd(Xd_max<Xd)=Xd_max(Xd_max<Xd);
+
+Xd(Xd_min>Xd)=Xd_min(Xd_min>Xd);
 
 end
 
--- matlab/main.m
+++ matlab/main.m
@@ -5,7 +5,7 @@ close all; clc; clear;
 %% Functions
 
 kpi = 1;
-kdi = 7;
+kdi = 5;
 d = 10;
 
 N = 3; % Platoon size with Leader
@@ -69,26 +69,64 @@ U_tilda = B*K*R;
 
 % X0  = [0 0 0 0 0 0 0 0 0 50]';
 X0  = zeros(N*2,1);
-X0(end) = 50;
+X0(end) = 22.352; % 50 mph in m/s
 
 
 % Xdot = @(t,X) (A_tilda * X + U_tilda);
 
-[t,X] = ode45(@(t,X) Xdot(t,X,A_tilda,U_tilda),[0 100],X0);
+[t,X] = ode45(@(t,X) Xdot(t,X,A_tilda,U_tilda),[0 30],X0);
 
 %% Plots
 U = -K*C*X'+K*R;
-for n=1:N
-figure(1); plot(t,X(:,n*2-1)); hold on
-end
+
+maxA = 13.4112; % 30 mph/s in m/s^2
+minA = -13.4112; % -30 mph/s m/s^2
+
+U(maxA<U)=maxA;
+U(-maxA>U)=maxA;
 
 for n=1:N
-figure(2); plot(t,X(:,n*2)); hold on
+figure(1); plot(t,X(:,n*2-1)); hold on;
 end
+title('Positions');
+xlabel('Time (s)');
+ylabel('Positions (m)');
+% legend
 
 for n=1:N
-figure(3); plot(t,U(n,:)); hold on
+figure(2); plot(t,X(:,n*2).*2.23694); hold on
 end
+
+title('Velocities');
+xlabel('Time (s)');
+ylabel('Velocities (mph)');
+% legend
+
+for n=1:N-1
+figure(3); plot(t,X(:,(n+1)*2-1)-X(:,(n)*2-1)); hold on;
+end
+title('Seperation');
+xlabel('Time (s)');
+ylabel('dP (m)');
+% legend
+
+for n=1:N-1
+figure(4); plot(t,X(:,(n+1)*2).*2.23694-X(:,n*2).*2.23694); hold on
+end
+
+title('Error in Velocities');
+xlabel('Time (s)');
+ylabel('dV (mph)');
+% legend
+
+for n=1:N
+figure(5); plot(t,U(n,:)./9.806); hold on
+end
+
+title('Accelerations');
+xlabel('Time (s)');
+ylabel('Accelerations (g)');
+% legend
 
 % figure; plot(t,U(1,:),t,U(2,:),t,U(3,:),t,U(4,:),t,U(5,:))
 
